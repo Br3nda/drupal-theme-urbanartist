eb74138978f18495b77597c409c0877f2b042503
Brenda Wallace <shiny@cpan.org>
2009-04-22 14:45:05 +1200

    the drupal theme

---
+++ page.tpl.php
@@ -0,0 +1,234 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+
+<head>
+
+<title>UrbanArtist</title>
+
+<meta http-equiv="content-type" content="application/xhtml+xml; charset=UTF-8" />
+<meta name="author" content="Erwin Aligam - styleshout.com" />
+<meta name="description" content="Site Description Here" />
+<meta name="keywords" content="keywords, here" />
+<meta name="robots" content="index, follow, noarchive" />
+<meta name="googlebot" content="noarchive" />
+
+<link rel="stylesheet" href="images/UrbanArtist.css" type="text/css" />
+
+</head>
+
+<body>
+
+<!-- wrap starts here -->
+<div id="wrap">
+
+	<!--header -->
+	<div id="header">			
+				
+		<h1 id="logo-text"><a href="index.html" title="">UrbanArtist</a></h1>		
+		<p id="intro">
+		Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+		Cras id urna. Morbi tincidunt, orci ac convallis aliquam. 
+		</p>	
+		
+		<div  id="nav">
+			<ul>
+				<li id="current"><a href="index.html">Home</a></li>
+				<li><a href="style.html">Style Demo</a></li>
+				<li><a href="index.html">Downloads</a></li>
+				<li><a href="index.html">Services</a></li>
+				<li><a href="index.html">Support</a></li>
+				<li><a href="index.html">About</a></li>		
+			</ul>		
+		</div>	
+						
+	<!--header ends-->					
+	</div>
+	
+	<!-- content-wrap starts -->
+	<div id="content-wrap">
+				
+		<div id="main">
+				
+			<a name="TemplateInfo"></a>
+			<h2><a href="index.html">Template Info</a></h2>
+			
+			<p class="post-info">Posted by <a href="index.html">erwin</a> | Filed under <a href="index.html">templates</a>, <a href="index.html">internet</a>  </p> 
+				
+			<p><strong>UrbanArtist 1.0</strong> is a free, W3C-compliant, CSS-based website template 
+			by <a href="http://www.styleshout.com/">styleshout.com</a>. This work is 
+			distributed under the <a rel="license" href="http://creativecommons.org/licenses/by/2.5/">
+			Creative Commons Attribution 2.5  License</a>, which means that you are free to 
+			use and modify it for any purpose. All I ask is that you include a link back to  
+			<a href="http://www.styleshout.com/">my website</a> in your credits.</p>  
+
+			<p>For more free designs, you can visit 
+			<a href="http://www.styleshout.com/">my website</a> to see 
+			my other works.</p>
+		
+			<p>Good luck and I hope you find my free templates useful!</p>
+				
+			<p class="postmeta">		
+			<a href="index.html" class="readmore">Read more</a> |
+			<a href="index.html" class="comments">Comments (7)</a> |				
+			<span class="date">June 22, 2008</span>	
+			</p>
+			
+			<h2><a href="index.html">Lorem Ipsum Dolor Sit Amet</a></h2>
+			
+			<h3>Aliquam Risus Justo</h3>
+			<p><a href="http://getfirefox.com/"><img src="images/firefox-gray.jpg" width="100" height="120" alt="firefox" class="float-left" /></a>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum 
+			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra 
+			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna. 
+			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. 				
+			</p>
+			
+			<blockquote>
+			<p>To be yourself in a world that is constantly trying to 
+			make you something else is the greatest accomplishment.
+			</p>
+			
+			<p class="align-right"> - Ralph Waldo Emerson</p>
+			</blockquote>
+			
+			<p>
+			Vestibulum venenatis. Nulla vel ipsum. Proin rutrum, urna sit amet bibendum 
+			pellentesque, ante sapien varius turpis, eu nonummy nunc urna scelerisque ante. 
+			Nunc sed leo at dolor pharetra facilisis. Sed sed lectus at justo dapibus tristique. 
+			Morbi molestie pulvinar elit. Donec dictum nibh nec est. Aliquam sagittis, lacus 
+			a pharetra tempor, enim justo tempor nisi, at aliquet nulla metus sed augue. 
+			Praesent sed nibh. Vestibulum bibendum enim ut nisi fringilla accumsan. 
+			Nullam dapibus fermentum nulla. Phasellus in orci vel justo facilisis iaculis. 
+			Quisque facilisis ornare felis. Curabitur nunc libero, dapibus vel, commodo nec, 
+			semper a, massa. Suspendisse potenti. Morbi a nisl. Nunc euismod elit non lectus. 				
+			</p>
+			
+			<h2><a href="index.html">Aliquam Risus Justo</a></h2>
+			
+			<h3>Lorem Ipsum Dolor Sit Amet</h3>
+			<p><a href="http://getfirefox.com/"><img src="images/firefox-gray.jpg" width="100" height="120" alt="firefox" class="float-right" /></a>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum 
+			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra 
+			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna. 
+			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. 				
+			</p>
+			
+			<h3>Lorem Ipsum</h3>
+			
+			<p>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum 
+			odio, ac blandit ante orci ut diam. Cras fringilla magna. Phasellus suscipit, leo a pharetra 
+			condimentum, lorem tellus eleifend magna, eget fringilla velit magna id neque. Curabitur vel urna. 
+			In tristique orci porttitor ipsum. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. 				
+			</p>			
+			
+		<!-- main ends -->	
+		</div>
+		
+		<!-- sidebar starts -->
+		<div id="sidebar">			
+		
+			<h3>About</h3>			
+			
+			<p>
+			<a href="http://getfirefox.com/"><img src="images/thumb.jpg" width="40" height="40" alt="firefox" class="float-left" /></a>
+			Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Donec libero. Suspendisse bibendum. 
+			Cras id urna. Morbi tincidunt, orci ac convallis aliquam, lectus turpis varius lorem, eu 
+			posuere nunc justo tempus leo. Donec mattis, purus nec placerat bibendum, dui pede condimentum 
+			odio, ac blandit ante orci ut diam.
+			</p>
+			
+			<form id="quick-search" action="index.html" method="get" >
+			<p>
+			<label for="qsearch">Search:</label>
+			<input class="tbox" id="qsearch" type="text" name="qsearch" value="Search this site..." title="Start typing and hit ENTER" />
+			<input class="btn" alt="Search" type="image" name="searchsubmit" title="Search" src="images/search.gif" />
+			</p>
+			</form>	
+		
+			<h3>Sidebar Menu</h3>
+			<ul class="sidemenu">				
+				<li><a href="index.html">Home</a></li>
+				<li><a href="index.html#TemplateInfo">TemplateInfo</a></li>
+				<li><a href="style.html">Style Demo</a></li>
+				<li><a href="http://www.styleshout.com/">More Free Templates</a></li>	
+				<li><a href="http://www.4templates.com/?aff=ealigam">Premium Templates</a></li>					
+			</ul>	
+				
+			<h3>Links</h3>
+			<ul class="sidemenu">
+				<li><a href="http://www.psdtuts.com/">PSDTuts</a></li>
+				<li><a href="http://www.alistapart.com">Alistapart</a></li>					
+				<li><a href="http://www.cssremix.com">CSS Remix</a></li>
+				<li><a href="http://www.cssmania.com/">CSS Mania</a></li>					
+			</ul>
+			
+			<h3>Sponsors</h3>
+			<ul class="sidemenu">
+				<li><a href="http://themeforest.net?ref=ealigam">ThemeForest <br /><span>Your Choice for Site Templates &amp; Wordpress Themes</span></a></li>
+				<li><a href="http://www.4templates.com/?aff=ealigam">4templates <br /><span>Low Cost Hi-Quality Templates</span></a></li>
+				<li><a href="http://store.templatemonster.com?aff=ealigam">TemplateMonster <br /><span>Delivering the Best Templates on the Net!</span></a></li>
+				<li><a href="http://tinyurl.com/3cgv2m">Text Link Ads <br /><span>Monetized your website</span></a></li>
+				<li><a href="http://www.fotolia.com/partner/114283">Fotolia <br /><span>Free stock images or from $1</span></a> </li>
+				<li><a href="http://www.dreamstime.com/res338619">Dreamstime <br /><span>Lowest Price for High Quality Stock Photos</span></a></li>
+				<li><a href="http://www.dreamhost.com/r.cgi?287326">Dreamhost <br /><span>Premium webhosting</span></a></li>
+			</ul>
+			
+			<h3>Wise Words</h3>
+			<p>&quot;To be yourself in a world that is constantly trying to make you 
+			  something else is the greatest accomplishment.&quot; </p>
+					
+			<p class="align-right">- Ralph Waldo Emerson</p>
+					
+			<h3>Support Styleshout</h3>
+			<p>If you are interested in supporting my work and would like to contribute, you are
+			welcome to make a small donation through the 
+			<a href="http://www.styleshout.com/">donate link</a> on my website - it will 
+			be a great help and will surely be appreciated.</p>						
+						
+		<!-- sidebar ends -->		
+		</div>
+		
+	<!-- content-wrap ends-->	
+	</div>
+
+	<!-- footer starts here -->	
+	<div id="footer">
+	
+		<div id="footer-left">
+			<p>
+			&copy; All your copyright info here	&nbsp; 	
+			Design by: <a href="http://www.styleshout.com/">styleshout</a>	&nbsp; 
+			
+			Valid: <a href="http://validator.w3.org/check/referer">XHTML</a> | 
+			<a href="http://jigsaw.w3.org/css-validator/check/referer">CSS</a>		
+			</p>	
+		</div>
+		
+		<div id="footer-right">
+			<p class="align-right">
+			<a href="index.html">Home</a> | <a href="index.html">Sitemap</a> | <a href="index.html" class="rssfeed">RSS Feed</a> 
+			</p>
+		</div>
+		
+	</div>
+	<!-- footer ends here -->
+	
+<!-- wrap ends here -->
+</div>
+
+</body>
+</html>
